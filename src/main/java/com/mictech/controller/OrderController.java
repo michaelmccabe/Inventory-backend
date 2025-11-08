a8cbd6a79b82c7c2738c2b91c888f43e59505b00
Michael McCabe <mccmichael@gmail.com>
2025-11-08 09:21:41 +0000

    add todos (#7)

--- src/main/java/com/mictech/controller/OrderController.java
+++ src/main/java/com/mictech/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController implements OrdersApi {
+    // TODO: add list and get endpoints
 
     private final OrderProcessor orderProcessor;
 
