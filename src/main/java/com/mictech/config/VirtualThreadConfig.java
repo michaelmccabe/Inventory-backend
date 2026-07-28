eab1bd3ed8194613d15f044e579bee11c8a65476
Michael McCabe <mccmichael@gmail.com>
2026-07-28 07:30:17 +0100

    update test containers version (#12)

--- src/main/java/com/mictech/config/VirtualThreadConfig.java
+++ src/main/java/com/mictech/config/VirtualThreadConfig.java
@@ -5,18 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.core.task.support.TaskExecutorAdapter;
-
+import java.util.Objects;
 import java.util.concurrent.Executors;
 
 @Configuration
-@ConditionalOnProperty(
-        value = "spring.threads.virtual.enabled",
-        havingValue = "true"
-)
+@ConditionalOnProperty(value = "spring.threads.virtual.enabled", havingValue = "true")
 public class VirtualThreadConfig {
 
     @Bean
     public AsyncTaskExecutor applicationTaskExecutor() {
-        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+        return new TaskExecutorAdapter(Objects.requireNonNull(Executors.newVirtualThreadPerTaskExecutor()));
     }
 }
